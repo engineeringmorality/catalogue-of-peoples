d84d91d74c507635c33be14e06b221d74ccc2c79
engineeringmorality <engineeringmorality@tuta.io>
2023-12-01 00:36:23 +0800

    feat(build): initial dataset and transformation script w/ Geonames

---
+++ build/transformation.js
@@ -0,0 +1,108 @@
+import {csvParse, csvFormat} from 'd3-dsv';
+import {readFileSync, writeFileSync} from 'fs';
+import {parse, format} from 'path';
+import Geonames from 'geonames.js';
+
+// TODO: also convert to GeoJson in this script? (reduce rendertime computation)
+// make the script take in a file from stdin?
+
+/** 
+ * @type {SupportForPalestine} 
+ * @NOTE (SENSITIVE API LIMIT): use the "development version" for testing. 
+ */
+// const csvFile = './data/support-for-palestine.csv'; // dataset for use in production/build
+const csvFile = './data/support-for-palestine.dev.csv';
+const geonamesUser = process.env.GEONAMES_USER;
+
+// The entry point of the script. 
+transformCsv(csvFile, geonamesUser);
+
+/**
+ * Appends latitude and longitude for each location, and writes to a new file.
+ * Said file is created in the same directory and should have the same name with prefix:`ready-`.
+ * @async
+ * @param {String} file The path of the file
+ * @param {String} username The Geonames account username
+ * @return {Promise<void>} Produces a side effect on happy path
+ */
+async function transformCsv(file, username) {
+  console.log(`running script with the file: "${file}" and user: "${geonamesUser}"`)
+  if (!geonamesUser) return console.error("Please set the GEONAMES_USER environment variable.");
+  const csv = getCsvData(file);
+  const geonames = new Geonames({username: username, lan: 'en', encoding: 'JSON'});
+  const transformedCsv = await addGeoDataToCsv(csv, geonames);
+
+  if (transformedCsv.includes(undefined)) return console.error("There seems to have been some error when fetching the geodata");
+	const path = parse(file);
+	const newFile = `${path.dir}/ready-${path.name}.csv`;
+	console.log('writing to file:',  newFile);
+	writeFileSync(newFile, csvFormat(transformedCsv));
+}
+
+/**
+ * @param {String} file The path of the file
+ * @return {DSVRowArray?}
+ * @todo the row transformer parameter of `csvParse`
+ */
+function getCsvData(file) {
+  try {
+    const contents = readFileSync(file, 'utf8');
+    const data = csvParse(contents);
+    console.info('data', data);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+/**
+ * @async
+ * @param {DSVRowArray} csv
+ * @param {GeonamesInstance} geonames
+ * @return {Promise<[DSVRowArray]>}
+ * @todo use a set data structure or memoize/filter duplicate locations
+ */
+async function addGeoDataToCsv(csv, geonames) {
+  const updatedRows = [];
+  for (const row of csv) {
+    try {
+      const latLng = await getLatLong(row.location, geonames);
+      if (!latLng) continue;
+      row.lat = latLng.lat; row.lng = latLng.lng;
+      console.info('newRow', row);
+      updatedRows.push(row);
+    } catch (err) {
+			console.error(err.response);
+      break; // stop makings calls on API error
+    }
+  }
+  return updatedRows;
+}
+
+/**
+ * @async
+ * @param {String} location - The name of the place
+ * @param {GeonamesInstance} geonames
+ * @return {Promise<Object?>}
+ * @todo create a `typedef` for the return value
+ */
+async function getLatLong(location, geonames) {
+	const geodata = await geonames.search({q: location, maxRows: 1, style: 'short'}); // `geocode` & `geoCodeAddress` only worked for few countries
+	const geoname = geodata.geonames[0];
+	if (geoname == undefined) {
+		console.error('Couldn\'t find geodata for the location:', location);
+	} else {
+		return {lat: geoname.lat, lng: geoname.lng};
+	}
+}
+
+// function getColumnData(file, column) {
+//   try {
+//     const contents = readFileSync(file, 'utf8');
+//     const data = csvParse(contents).map(row => row[column]);
+//     console.info("data", data);
+//     return data;
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
---
+++ data/support-for-palestine.d.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef {Object} SupportForPalestine
+ * @property {String} date - Format: d/m/y
+ * @property {String} location - The name of the place (usually a city)
+ * @property {Url} video - URL to the video showing support
+ * @property {ENotation} figure - Or, exponent. Example: since 5000 = 5*10^3, use '3'
+ * @property {Url} [source] - The source of the `figure`. Can be empty if the same as `video`
+ */
