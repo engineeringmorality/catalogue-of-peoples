709616aef986cfa07023ea2d5513dfb83789dae6
engineeringmorality <engineeringmorality@tuta.io>
2023-12-19 17:08:22 +0800

    fix: globe point of view and docs

--- data/support-for-palestine.d.js
+++ data/support-for-palestine.d.js
@@ -2,7 +2,7 @@
  * Dataset of different instances of global demonstration of support for the Palestinian cause, since the start of the latest imperial assault of the peoples of Gaza.
  * @typedef {Object} SupportForPalestine
  * @property {String} date - Format: d/m/y
- * @property {String} location - The name of the place (usually a city)
+ * @property {String} location - The name of the place (usually a city); here, name of the country has been written first for easier sorting.
  * @property {Url} video - URL to the video showing support
  * @property {ENotation} figure - Or, exponent. Example: since 5000 = 5*10^3, use '3'
  * @property {Url} [source] - The source of the `figure`. Can be empty if the same as `video`
--- src/globe.js
+++ src/globe.js
@@ -10,6 +10,7 @@ fetch(data).then(res => res.text()).then(places => {
 	const csvData = csvParse(places, ({lat, lng, location, figure, video}) => ({lat: +lat, lng: +lng, figure: +figure || 1, location: location, video: video}));
 	console.log("csvData", csvData);
 	globe(document.getElementById('globeViz'))
+		.pointOfView({lat:24})
 		.globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
 		.backgroundImageUrl('//unpkg.com/three-globe/example/img/night-sky.png')
 		.labelColor(() => 'rgba(255, 255, 255, 0.50)')
@@ -19,8 +20,6 @@ fetch(data).then(res => res.text()).then(places => {
 		.labelLng(d => d.lng)
 		.labelDotRadius(d => d.figure * 4e-1)
 		.labelSize(d => d.figure * 4e-1)
-		// .labelDotRadius(d => Math.sqrt(+`1e${d.figure}`) * 8e-3)
-		// .labelSize(d => Math.sqrt(+`1e${d.figure}`) * 8e-3)
 		.labelText(d => d.location.split(',').pop());
 
 	const controls = globe.controls();
